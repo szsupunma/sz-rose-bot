e19c44348608295755d487a05f26f7d5d8c0c31c
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-09-24 20:39:11 +0530

    lol

--- SZRoseBot/errors.py
+++ SZRoseBot/errors.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from functools import wraps
-from DewmiBot import pbot, SUPPORT_CHAT
+from SZRoseBot import pbot, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
