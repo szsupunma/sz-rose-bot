dfa823773295a0f0a263ade7b83fae614ad805cc
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-09-24 20:52:15 +0530

    ok

--- SZRoseBot/Best_Of_ SZRoseBot/pluginshelper.py
+++ SZRoseBot/Best_Of_ SZRoseBot/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from DewmiBot import OWNER_ID, SUPPORT_CHAT
-from DewmiBot import pbot
+from SZRoseBot import OWNER_ID, SUPPORT_CHAT
+from SZRoseBot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
