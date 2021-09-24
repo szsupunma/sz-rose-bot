0eed37ddd65dccae08403945c00d597f54b49fc9
CGS-TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-09-24 18:50:35 +0530

    Update sample_config.py

--- DewmiBot/sample_config.py
+++ DewmiBot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/tg_bot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/DewmiBot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "API_HASH"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1807441991  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "kmsrk"
-    SUPPORT_CHAT = "cgs_official"  # Your own group for support, do not add the @
+    OWNER_ID = 123456  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "hirunaoficial"
+    SUPPORT_CHAT = "sltechzoneofficial"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
