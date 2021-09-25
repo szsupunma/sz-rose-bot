b8c324c298a0be6eeff70fa5dc83c21de79a6e45
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-24 17:43:49 -0700

    Update sample_config.py

--- DewmiBot/sample_config.py
+++ DewmiBot/sample_config.py
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "API_HASH"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 123456  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "hirunaoficial"
-    SUPPORT_CHAT = "sltechzoneofficial"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "supunma"
+    SUPPORT_CHAT = "slbotzone"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
