5d47ee62fd4521b67593b8d8405983ce7a7fc89f
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 11:45:03 +0530

    Update reporting.py

--- DewmiBot/modules/reporting.py
+++ DewmiBot/modules/reporting.py
@@ -289,7 +289,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "Reporting"
+__mod_name__ = "ʀᴇᴘᴏʀᴛɪɴɢ🔂"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
