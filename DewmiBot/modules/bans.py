cc70f31cbce8b9195587de0552798f8da978dca9
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 10:13:42 +0530

    Update bans.py

--- DewmiBot/modules/bans.py
+++ DewmiBot/modules/bans.py
@@ -632,7 +632,7 @@ dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 dispatcher.add_handler(SBAN_HANDLER)
 
-__mod_name__ = "Bans"
+__mod_name__ = "ʙᴀɴ🚫"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
