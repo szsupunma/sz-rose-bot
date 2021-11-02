1e7172314166b34dd8f68acf09d217e434f2da47
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 11:00:00 +0530

    Update cust_filters.py

--- DewmiBot/modules/cust_filters.py
+++ DewmiBot/modules/cust_filters.py
@@ -633,7 +633,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "ꜰɪʟᴛᴇʀ📇"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
