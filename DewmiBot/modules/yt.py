7fcad2b872810ec345723116d66e73470f205356
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 12:02:02 +0530

    Update yt.py

--- DewmiBot/modules/yt.py
+++ DewmiBot/modules/yt.py
@@ -3,4 +3,4 @@ __help__ = """
  ❍ /video <text>: perform a youtube search
  ❍ /song <link> or /video <link>: Downlods a video or audio from a youtube video to the bots local server
 """
-__mod_name__ = "Youtube"
+__mod_name__ = "ʏᴏᴜᴛᴜʙᴇ📺"
