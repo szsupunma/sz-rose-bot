6408c3d78bc43bd962dd974fcbfeb32d55bcaba1
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 11:31:41 +0530

    Update muting.py

--- DewmiBot/modules/muting.py
+++ DewmiBot/modules/muting.py
@@ -393,7 +393,7 @@ dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 dispatcher.add_handler(STEMPMUTE_HANDLER)
 
-__mod_name__ = "Muting "
+__mod_name__ = "ᴍᴜᴛɪɴɢ🔕"
 __handlers__ = [
     MUTE_HANDLER,
     SMUTE_HANDLER,
