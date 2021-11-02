8b5c5e963e9cb622460ec539e06b5aeba9fc0eba
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 11:14:12 +0530

    Update locks.py

--- DewmiBot/modules/locks.py
+++ DewmiBot/modules/locks.py
@@ -578,7 +578,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  
 """
 
-__mod_name__ = "Locks"
+__mod_name__ = "ʟᴏᴄᴋꜱ🔏"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
