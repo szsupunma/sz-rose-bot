24b6b235b904e3425e20c6beefd4caae0b606639
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 11:30:12 +0530

    Update misc.py

--- DewmiBot/modules/misc.py
+++ DewmiBot/modules/misc.py
@@ -656,7 +656,7 @@ __help__ = """
 \
 """
 
-__mod_name__ = "Miscs"
+__mod_name__ = "ᴍɪꜱᴄꜱ🧫"
 
 APP_HANDLER = DisableAbleCommandHandler("app", app)
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
