f1a92a56b33f4c583e2f1612fabcd82fe213ae1e
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-11-02 09:33:34 +0530

    Update approve.py

--- DewmiBot/modules/approve.py
+++ DewmiBot/modules/approve.py
@@ -227,6 +227,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Approval"
+__mod_name__ = "ᴀᴘᴘʀᴏᴠᴀʟ🥡"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
